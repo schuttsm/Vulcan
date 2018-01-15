4f16d1354d2cff50ed9253358c459565fba37fd1
Stephen Schutt <stephen.schutt@cornerstonenw.com>
2018-01-15 14:12:52 +0100

    add a webdriver/selenium test prototype.  npm test will setup & spawn meteor, selenium standalone & a webdriver process that runs through 4 tests in example-simple.  Intended as a proof-of-concept of how to implement an integration test suite on vulcan

---
+++ tests/integration/example-simple/selenium-tests.js
@@ -0,0 +1,64 @@
+const wdio = require('wdio');
+const chai = require('chai');
+
+describe('integration tests', function() {
+  this.timeout(1000 * 60);
+  var browser;
+  var testUser = 'testUser', testPwd = 'testPwd', testEmail = 'test@test.com';
+  before(function() {
+    browser = wdio.getBrowser({desiredCapabilities: {browserName: 'chrome'}});
+    browser.init();
+    browser.url('http://localhost:3000');
+    browser.waitForVisible('#usernameOrEmail');
+    chai.assert.equal('Your site title', browser.getTitle());
+  });
+
+  after(function() {
+    browser.end();
+  });
+
+  it('should signup correctly', function() {
+    chai.assert.deepEqual(browser.getText('.buttons a:nth-child(2)'), 'Sign up');
+    browser.click('.buttons a:nth-child(2)');
+    browser.waitForVisible('#username');
+    browser.setValue('#username', testUser);
+    browser.setValue('#email', testEmail);
+    browser.setValue('#password', testPwd);
+    browser.click('[type="submit"]');
+    browser.waitUntil(function () {
+      return browser.getText('.buttons a') == 'Change password';
+    }, 5000, 'Change button did not appear 5s after login');
+    chai.assert.deepEqual(browser.getText('.btn-primary'), ['Sign out', 'Submit']);
+  });
+
+  it('should signout correctly', function() {
+    browser.click('.btn-primary:nth-child(1)');
+    browser.waitForVisible('#usernameOrEmail');
+  });
+
+  it('should prevent a second login of the same user', () => {
+    chai.assert.deepEqual(browser.getText('.btn-primary'), 'Sign in');
+    browser.click('.buttons a:nth-child(2)');
+    browser.waitForVisible('#username');
+    browser.setValue('#username', testUser);
+    browser.setValue('#email', testEmail);
+    browser.setValue('#password', testPwd);
+    browser.click('[type="submit"]');
+    browser.waitForVisible('.message.error');
+    chai.assert.equal(browser.getText('.message.error'), 'Username already exists');
+  });
+
+  it('should login correctly', function() {
+    chai.assert.deepEqual(browser.getText('.buttons a:nth-child(2)'), 'Sign in');
+    browser.click('.buttons a:nth-child(2)');
+    browser.waitForVisible('#usernameOrEmail');
+    browser.setValue('#usernameOrEmail', testEmail);
+    browser.setValue('#password', testPwd);
+    browser.click('[type="submit"]');
+    browser.waitUntil(function () {
+      return browser.getText('.buttons a') == 'Change password';
+    }, 5000, 'Change button did not appear 5s after login');
+    chai.assert.equal(browser.getText('.buttons a'), 'Change password');
+    chai.assert.deepEqual(browser.getText('.btn-primary'), ['Sign out', 'Submit']);
+  });
+});
---
+++ tests/integration/run-tests.js
@@ -0,0 +1,110 @@
+var path = require('path');
+var kill = require('kill-process'),
+  selenium = require('selenium-standalone'),
+  spawn = require('child_process').spawn;
+
+var procs = [];
+
+function closeProcs() {
+  console.log('closing procs');
+  procs.forEach(function(proc) {
+    kill(proc.pid);
+  });
+  return;
+}
+
+process.on('unhandledRejection', (reason, p) => {
+  console.error(reason, 'Unhandled Rejection at Promise', p);
+  closeProcs();
+  process.exit(1);
+}).on('uncaughtException', err => {
+  console.error(err, 'Uncaught Exception thrown');
+  closeProcs();
+  process.exit(1);
+}).on('SIGINT', function() {
+  console.log('Caught interrupt signal');
+  closeProcs();
+  process.exit();
+});
+
+const setPackage = package_name => {
+  return new Promise((resolve, reject) => {
+    var proc = spawn('meteor.bat', ['add', package_name], { stdio: 'inherit' });
+    proc.on('close', function() {
+      resolve();
+    });
+  });
+};
+
+const resetMeteor = () => {
+  return new Promise((resolve, reject) => {
+    var proc = spawn('meteor.bat', ['reset'], { stdio: 'inherit' });
+    proc.on('close', function() {
+      return resolve();
+    });
+  });
+};
+
+const startMeteor = () => {
+  return new Promise((resolve, reject) => {
+    var proc = spawn('meteor.bat', ['--settings', 'settings.json']);
+    procs.push(proc);
+    proc.stdout.on('data', (data) => {
+      var line = data.toString();
+      console.log(line);
+      if (line.indexOf('localhost:3000') != -1) {
+        console.log('Meteor server Successfully started');
+        resolve();
+      }
+    });
+    proc.stderr.on('data', (data) => {
+      console.log(data.toString());
+      reject(new Error(data.toString()));
+    });
+  });
+};
+
+const startSelenium = () => {
+  return new Promise((resolve, reject) => {
+    console.log('starting selenium');
+    selenium.start({spawnOptions: {stdio: 'inherit'}}, (err, selenium_proc) => {
+      if (err) {
+        console.error('Error starting selenium: ');
+        console.error(err);
+        return reject(err);
+      }
+      procs.push(selenium_proc);
+
+      console.log('selenium started');
+      resolve();
+    });
+  });
+}
+
+
+const runTestPackage = ((package_name, callback) => {
+  return new Promise((resolve, reject) => {
+    var wdio_conf_file = path.join(__dirname, package_name, 'wdio.conf.js');
+    console.log('running test package: ' + wdio_conf_file);
+    var wdio_proc = spawn(path.join(__dirname, '..', '..', 'node_modules', '.bin', 'wdio.cmd'), [wdio_conf_file], {'stdio': 'inherit'});
+    wdio_proc.on('error', function(data) {
+      return reject(new Error(data.toString()));
+    });
+    wdio_proc.on('close', resolve);
+  });
+});
+
+var package_name = 'example-simple';
+setPackage(package_name)
+  .then(resetMeteor)
+  .then(startMeteor)
+  .then(startSelenium)
+  .then(runTestPackage.bind(null, package_name))
+  .catch((reason) => {
+    console.error(reason);
+    console.log('Error running ' + __filename);
+  })
+  .then(() => {
+    closeProcs();
+    console.log('Successfully run tests');
+  });
