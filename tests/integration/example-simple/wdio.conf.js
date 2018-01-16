cfbfce4dbb99c854f069fc7f32312a58fb125491
Stephen Schutt <stephen.schutt@cornerstonenw.com>
2018-01-16 14:45:47 +0100

    try chrome in no sandbox mode to get it working with travis

--- tests/integration/example-simple/wdio.conf.js
+++ tests/integration/example-simple/wdio.conf.js
@@ -44,7 +44,10 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 1,
         //
-        browserName: 'chrome'
+        browserName: 'chrome',
+        chromeOptions: {
+          args: ['--no-sandbox']
+        }
     }],
     //
     // ===================
